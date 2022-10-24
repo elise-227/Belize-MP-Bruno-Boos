f5cac9b32bbf2721db6ea2542ae597f3c112f448
kelbomb <kelly.a.bruno@duke.edu>
2022-10-24 15:41:29 -0400

    coding!

--- Kelly Coding/Analysis_kelly.R
+++ Kelly Coding/Analysis_kelly.R
@@ -1,3 +1,22 @@
+#clean space
+#load packages
+#packages
+require(tidyverse)
+require(dplyr)
+require(sf)
+require(leaflet)
+require(mapview)
+require(readxl)
+require(lubridate)
+require(readxl)
+library(unmarked)
+library(AICcmodavg)
+library(camtrapR)
+library(ubms)
+#set wd
+
 ##load dfs
 load("obs_data.RData")
 load("cam_op.RData")
+
+#make camera_operation frame
